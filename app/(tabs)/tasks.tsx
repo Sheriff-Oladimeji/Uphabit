6c7a41879117b77079c0aa7769d644e5fbb33e3b
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-09-03 06:45:30 +0100

    initial commit

---
+++ app/(tabs)/tasks.tsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const tasks = () => {
+  return (
+    <View>
+      <SafeAreaView style={{margin: 50}}>
+        <Text style={{ color: "white" }}>tasks</Text>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+export default tasks
